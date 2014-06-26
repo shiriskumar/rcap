f80913764512c9c8fcbe686143b005046d943497
0x90 <oleg.kupreev@gmail.com>
2014-06-26 20:00:41 +0400

    initial commit

--- rcap.sh
+++ rcap.sh
@@ -6,13 +6,15 @@ ROUTER_IP="1.0.0.1"
 ROUTER_IFACE="br-lan"
 ###########################
 
-if [ -f $PIPE ];
-then
-   echo "$PIPE exists."
-else
-   echo "File $PIPE does not exist. Creating..."
-   mkfifo $PIPE
-fi
+#if [ -f $PIPE ];
+#then
+#   echo "$PIPE exists."
+#else
+#   echo "File $PIPE does not exist. Creating..."
+#
+#fi
+
+mkfifo $PIPE
 
 echo "Starting capture on $ROUTER_IP"
 ssh root@$ROUTER_IP "tcpdump -i $ROUTER_IFACE -s 0 -U -w - not port 22" > $SNIFF_PIPE
