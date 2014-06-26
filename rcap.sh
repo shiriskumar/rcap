c133ee05f6703e2c4332832ee84046d907099570
0x90 <oleg.kupreev@gmail.com>
2014-06-26 20:00:04 +0400

    initial commit

--- rcap.sh
+++ rcap.sh
@@ -17,6 +17,6 @@ fi
 echo "Starting capture on $ROUTER_IP"
 ssh root@$ROUTER_IP "tcpdump -i $ROUTER_IFACE -s 0 -U -w - not port 22" > $SNIFF_PIPE
 
-echo "Launching Wireshark"
-wireshark -k -i $SNIFF_PIPE
+echo "To run Wireshark exec:"
+echo "wireshark -k -i $SNIFF_PIPE"
 
